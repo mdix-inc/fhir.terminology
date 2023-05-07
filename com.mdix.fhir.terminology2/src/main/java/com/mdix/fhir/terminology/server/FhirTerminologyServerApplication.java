5bf2a8358ce1a4729c44503d83f6be8058ed6f86
Sean Muir <sean.muir@jkmsoftware.com>
2023-05-07 16:13:21 -0400

    Updates

--- com.mdix.fhir.terminology2/src/main/java/com/mdix/fhir/terminology/server/FhirTerminologyServerApplication.java
+++ com.mdix.fhir.terminology2/src/main/java/com/mdix/fhir/terminology/server/FhirTerminologyServerApplication.java
@@ -5,6 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import javax.annotation.PostConstruct;
@@ -18,6 +19,7 @@ import org.hl7.fhir.dstu3.model.UriType;
 import org.hl7.fhir.exceptions.FHIRFormatError;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -26,6 +28,9 @@ import com.mdix.fhir.terminology.server.provider.ConceptMapProvider;
 @SpringBootApplication
 public class FhirTerminologyServerApplication {
 
+	@Value("#{systemProperties['fhir.conceptmaps'] ?: '/conceptmaps'}")
+	private String conceptmapsFolders;
+
 	static Logger logger = LoggerFactory.getLogger(FhirTerminologyServerApplication.class);
 
 	public static void main(String[] args) {
@@ -33,17 +38,7 @@ public class FhirTerminologyServerApplication {
 	}
 
 	@PostConstruct
-	public static void initialize() {
-		try {
-			loadConceptMaps("/v2tofhir");
-			loadConceptMaps("/localmaps");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-	}
-
-	static void loadConceptMaps(String pathToLoad) throws IOException {
+	public void initialize() {
 
 		Consumer<? super Path> loadConceptMap = new Consumer<Path>() {
 
@@ -123,10 +118,275 @@ public class FhirTerminologyServerApplication {
 			}
 		};
 
-		try (Stream<Path> paths = Files.walk(Paths.get(pathToLoad))) {
+		try {
+			for (String mapsFolder : Stream.of(conceptmapsFolders.split(",", -1)).collect(Collectors.toList())) {
+				try (Stream<Path> paths = Files.walk(Paths.get(mapsFolder))) {
+					paths.filter(Files::isRegularFile).forEach(loadConceptMap);
+				}
+			}
+		} catch (IOException e) {
 
-			paths.filter(Files::isRegularFile).forEach(loadConceptMap);
+			e.printStackTrace();
 		}
+
+		//
+		// Set<Path> folders = Files.find(
+		// Paths.get(mapsFolder), Integer.MAX_VALUE, (filePath, fileAttr) -> fileAttr.isDirectory()).collect(
+		// Collectors.toSet());
+		//
+		// for (Path folder : folders) {
+		//
+		// try (Stream<Path> paths = Files.walk(folder)) {
+		//
+		// paths.filter(Files::isRegularFile).forEach(loadConceptMap);
+		// }
+		//
+		// // Set<String> maps = Stream.of(new File(folder.toString()).listFiles()).filter(
+		// // file -> (!file.isDirectory() && file.toString().endsWith("mdmi"))).map(File::getName).collect(
+		// // Collectors.toSet());
+		// // for (String map : maps) {
+		// // logger.trace("Loading conceptmapsFolders " + map);
+		// // InputStream targetStream = new FileInputStream(folder.toString() + "/" + map);
+		// //
+		// //
+		// //
+		// // logger.trace("Loaded conceptmapsFolders " + map);
+		// // }
+		// }
+		// }
+		//
+		// } catch (IOException e) {
+		//
+		// e.printStackTrace();
+		// }
+		// try {
+		// for (String mapsFolder : Stream.of(conceptmapsFolders.split(",", -1)).collect(Collectors.toList())) {
+		//
+		// Set<Path> folders = Files.find(
+		// Paths.get(mapsFolder), Integer.MAX_VALUE, (filePath, fileAttr) -> fileAttr.isDirectory()).collect(
+		// Collectors.toSet());
+		//
+		// }
+		// } catch (IOException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+		// try {
+		// loadConceptMaps("/v2tofhir");
+		// loadConceptMaps("/localmaps");
+		// } catch (IOException e) {
+		// e.printStackTrace();
+		// }
+
 	}
+
+	// private void loadMaps() throws IOException {
+	// synchronized (this) {
+	//
+	// if (loaded || lastModified == 0) {
+	// long currentModified = 0;
+	// for (String mapsFolder : Stream.of(mapsFolders.split(",", -1)).collect(Collectors.toList())) {
+	//
+	// Set<Path> folders = Files.find(
+	// Paths.get(mapsFolder), Integer.MAX_VALUE,
+	// (filePath, fileAttr) -> fileAttr.isDirectory()).collect(Collectors.toSet());
+	//
+	// for (Path folder : folders) {
+	//
+	// Set<File> maps2 = Stream.of(new File(folder.toString()).listFiles()).filter(
+	// file -> (!file.isDirectory() && file.toString().endsWith("mdmi"))).collect(
+	// Collectors.toSet());
+	//
+	// for (File map : maps2) {
+	// if (map.lastModified() > currentModified) {
+	// currentModified = map.lastModified();
+	// }
+	//
+	// }
+	//
+	// }
+	// }
+	//
+	// if (currentModified > lastModified) {
+	// loaded = false;
+	// mapProperties.clear();
+	// preprocessors.clear();
+	// postprocessors.clear();
+	// targetsemanticprocessors.clear();
+	// sourcesemanticprocessors.clear();
+	// lastModified = currentModified;
+	//
+	// }
+	//
+	// }
+	//
+	// if (loaded) {
+	// return;
+	// }
+	//
+	// FHIRTerminologyTransform.codeValues.clear();
+	//
+	// FHIRTerminologyTransform.processTerminology = true;
+	//
+	// FHIRTerminologyTransform.setFHIRTerminologyURL(terminologySettings.getUrl());
+	//
+	// FHIRTerminologyTransform.setUserName(terminologySettings.getUserName());
+	//
+	// FHIRTerminologyTransform.setPassword(terminologySettings.getPassword());
+	//
+	// for (String mapsFolder : Stream.of(mapsFolders.split(",", -1)).collect(Collectors.toList())) {
+	//
+	// Set<Path> folders = Files.find(
+	// Paths.get(mapsFolder), Integer.MAX_VALUE, (filePath, fileAttr) -> fileAttr.isDirectory()).collect(
+	// Collectors.toSet());
+	//
+	// for (Path folder : folders) {
+	//
+	// Set<String> maps = Stream.of(new File(folder.toString()).listFiles()).filter(
+	// file -> (!file.isDirectory() && file.toString().endsWith("mdmi"))).map(File::getName).collect(
+	// Collectors.toSet());
+	// for (String map : maps) {
+	// logger.trace("Loading map " + map);
+	// InputStream targetStream = new FileInputStream(folder.toString() + "/" + map);
+	// Mdmi.INSTANCE().getResolver().resolve(targetStream);
+	// logger.trace("Loaded map " + map);
+	// }
+	//
+	// System.err.println(folder.toString() + "/terms");
+	//
+	// Set<String> datatypeterms = Stream.of(new File(folder.toString() + "/terms").listFiles()).filter(
+	// file -> (!file.isDirectory() && file.toString().endsWith("properties"))).map(
+	// File::getName).collect(Collectors.toSet());
+	// for (String datatypeterm : datatypeterms) {
+	// logger.trace("Loading datatypeterm " + datatypeterm);
+	// InputStream datatypetermStream = new FileInputStream(
+	// folder.toString() + "/terms/" + datatypeterm);
+	// Utils.mapOfTransforms.put(FilenameUtils.removeExtension(datatypeterm), new Properties());
+	// Utils.mapOfTransforms.get(FilenameUtils.removeExtension(datatypeterm)).load(datatypetermStream);
+	// // Mdmi.INSTANCE().getResolver().resolve(targetStream);
+	// logger.trace("Loaded map " + datatypeterm);
+	// }
+	//
+	// logger.trace("Check for processors.yml ");
+	// logger.trace("Looking for " + folder.toString() + "/" + "processors.yml");
+	// logger.trace("EXISTS " + Files.exists(Paths.get(folder.toString() + "/" + "processors.yml")));
+	// if (Files.exists(Paths.get(folder.toString() + "/" + "processors.yml"))) {
+	// logger.trace("Found processors.yml ");
+	// Yaml processorYaml = new Yaml();
+	// InputStream inputStream = new FileInputStream(folder.toString() + "/" + "processors.yml");
+	// Map<String, Object> obj = processorYaml.load(inputStream);
+	//
+	// if (obj.containsKey("preprocessors")) {
+	// preprocessors.add((Map<String, Object>) obj.get("preprocessors"));
+	// }
+	// if (obj.containsKey("postprocessors")) {
+	// postprocessors.add((Map<String, Object>) obj.get("postprocessors"));
+	// }
+	//
+	// if (obj.containsKey("sourcesemanticprocessors")) {
+	// sourcesemanticprocessors.add((Map<String, Object>) obj.get("sourcesemanticprocessors"));
+	// }
+	//
+	// if (obj.containsKey("targetsemanticprocessors")) {
+	// targetsemanticprocessors.add((Map<String, Object>) obj.get("targetsemanticprocessors"));
+	// }
+	//
+	// }
+	//
+	// }
+	//
+	// }
+	//
+	// loaded = Boolean.TRUE;
+	//
+	// }
+	//
+	// }
+
+	// static void loadConceptMaps(String pathToLoad) throws IOException {
+	//
+	// Consumer<? super Path> loadConceptMap = new Consumer<Path>() {
+	//
+	// @Override
+	// public void accept(Path path) {
+	//
+	// ConceptMap conceptMapFromTo = new ConceptMap();
+	// ConceptMap conceptMapToFrom = new ConceptMap();
+	// ConceptMapGroupComponent cmgcFromTo = conceptMapFromTo.addGroup();
+	// ConceptMapGroupComponent cmgcToFrom = conceptMapToFrom.addGroup();
+	// int count = 0;
+	// boolean isValid = false;
+	//
+	// try {
+	// for (String line : Files.readAllLines(path)) {
+	//
+	// String[] code2code = line.toString().split("\t");
+	// if (count < 2) {
+	// count++;
+	// continue;
+	// }
+	// if (code2code.length == 10) {
+	//
+	// UriType sourceuri = new UriType();
+	// sourceuri.setValue(code2code[2]);
+	//
+	// conceptMapFromTo.setSource(sourceuri);
+	// conceptMapToFrom.setTarget(sourceuri);
+	//
+	// UriType targeturi = new UriType();
+	// targeturi.setValue(code2code[9]);
+	//
+	// conceptMapFromTo.setTarget(targeturi);
+	// conceptMapToFrom.setSource(targeturi);
+	//
+	// cmgcFromTo.setSource(code2code[2]);
+	// cmgcFromTo.setTarget(code2code[9]);
+	//
+	// cmgcToFrom.setTarget(code2code[2]);
+	// cmgcToFrom.setSource(code2code[9]);
+	//
+	// if ((code2code[0] != null && !code2code[0].contentEquals("")) &&
+	// (code2code[6] != null && !code2code[6].contentEquals(""))) {
+	// SourceElementComponent secFromTo = cmgcFromTo.addElement();
+	// CodeType aaa = new CodeType();
+	// secFromTo.setCodeElement(aaa);
+	// secFromTo.setCode(code2code[0]).addTarget().setCode(code2code[6]).setEquivalence(
+	// ConceptMapEquivalence.EQUAL);
+	//
+	// SourceElementComponent secToFrom = cmgcToFrom.addElement();
+	// CodeType aaa2 = new CodeType();
+	// secToFrom.setCodeElement(aaa2);
+	// secToFrom.setCode(code2code[6]).addTarget().setCode(code2code[0]).setEquivalence(
+	// ConceptMapEquivalence.EQUAL);
+	// isValid = true;
+	// }
+	// } else {
+	// logger.error(
+	// "Incomplete mapping " + path.getFileName() + " At line " + String.valueOf(count) +
+	// " :: " + line);
+	// }
+	// }
+	// if (isValid) {
+	// ConceptMapProvider.addConceptMap(conceptMapToFrom);
+	// ConceptMapProvider.addConceptMap(conceptMapFromTo);
+	// logger.info("Loaded Mapping File " + path.getFileName());
+	// } else {
+	// logger.info("Did not load Mapping File " + path.getFileName());
+	//
+	// }
+	//
+	// } catch (FHIRFormatError e) {
+	// logger.error("Error loading " + path.getFileName(), e);
+	// } catch (IOException e) {
+	// logger.error("Error loading " + path.getFileName(), e);
+	// }
+	// }
+	// };
+	//
+	// try (Stream<Path> paths = Files.walk(Paths.get(pathToLoad))) {
+	//
+	// paths.filter(Files::isRegularFile).forEach(loadConceptMap);
+	// }
+	// }
 
 }
--- com.mdix.fhir.terminology2/src/test/java/com/mdmix/fhir/terminology/server/test/TerminologyTest.java
+++ com.mdix.fhir.terminology2/src/test/java/com/mdmix/fhir/terminology/server/test/TerminologyTest.java
@@ -11,26 +11,62 @@
  *******************************************************************************/
 package com.mdmix.fhir.terminology.server.test;
 
-import org.junit.jupiter.api.Test;
+import java.util.HashMap;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.mdix.fhir.terminology.server.FhirTerminologyServerApplication;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = FhirTerminologyServerApplication.class, webEnvironment = WebEnvironment.RANDOM_PORT)
 public class TerminologyTest {
 
 	@Autowired
 	private TestRestTemplate template;
 
+	@BeforeAll
+	public static void setEnvironment() {
+		System.setProperty("fhir.conceptmaps", "src/main/resources/conceptmaps");
+	}
+
 	@Test
 	public void testGetConceptMaps() {
-		ResponseEntity<String> response = template.getForEntity("fhir/ConceptMap", String.class);
+
+		System.out.println(template.getRootUri());
+
+		ResponseEntity<String> response = template.getForEntity("/fhir/ConceptMap", String.class);
 		// assertTrue(response.getStatusCode().equals(HttpStatus.OK));
 		System.out.println(response.getBody());
+	}
+
+	@Test
+	public void testTransform() {
+
+		// ;template.get
+
+		HashMap<String, Object> params = new HashMap<>();
+		params.put("source", "http://hl7.org/fhir/name-use");
+		params.put("target", "HL70200");
+		params.put("code", "official");
+		// Object params;
+		ResponseEntity<String> response = template.getForEntity(
+			"/fhir/ConceptMap/$translate?source=http://hl7.org/fhir/name-use&target=HL70200&code=official&system=system",
+			String.class, params);
+		// assertTrue(response.getStatusCode().equals(HttpStatus.OK));
+		System.out.println(response.getBody());
+
+		// Unirest.setTimeouts(0, 0);
+		// HttpResponse<String> response = Unirest.get(
+		// "localhost:8080/fhir/ConceptMap/$translate?source=&target=HL70200&code=official&system=system").asString();
+
 	}
 
 }
